4bf272a582adbbf4724d70a286031e6b8d0985ed
djonatanmqp <djonatanmqp@gmail.com>
2022-12-13 17:45:53 -0300

    Aula 7

---
+++ betabase_dinamico/app/views/footer.php
@@ -0,0 +1,47 @@
+<!-- MODAL HELP -->
+<div class="modal fade" id="help" tabindex="-1" role="dialog" aria-labelledby="help">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Ajuda</h4>
+      </div>
+      <div class="modal-body">
+        <p style="text-align:center"><img src="app/img/logo.svg" alt="logo betabase"></p><br>
+        <p>Betabase é uma base de dados com todas as sequências e estruturas de beta-glicosidases
+        	disponíveis no PDB.</p>
+          <p>Pressione <b>navegar</b> para obter uma lista de beta-glicosidases disponíveis. Para buscar
+           novas sequências, clique no menu <b>BLAST</b>. Para mais informações, entre em contato com 
+           <a href="#">Prof. Djonatan Piehowiak</a>.
+         </p><br>
+         <p><label class="label label-default">Atualizado em:</label> 12 de dezembro, 2022</p>
+       </div>
+       <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- FIM Modal -->
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"><a href="?i=home"><img id="logo" src="app/img/logo.svg"></a></div>
+            <div class="col-md-1 lista_footer"><a href="?i=sobre">Sobre</a></div>
+            <div class="col-md-1 lista_footer"><a href="#">Listar</a></div>
+            <div class="col-md-1 lista_footer"><a href="#">Buscar</a></div>
+            <div class="col-md-1 lista_footer"><a href="#" data-toggle="modal" data-target="#help">Ajuda</a></div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p>© 2022 betabase | Todos os direitos reservados. Construido por <a href="#">Prof. Djonatan Piehowiak</a>.</p>
+            </div>
+        </div>
+    </div>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="app/js/bootstrap.min.js"></script>
+</footer>
+</body>
+</html>
